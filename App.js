adfb4d7fb69ae867e66e80c7a7c7936a6412b363
Prawito Hudoro <prawitohudoro@gmail.com>
2021-03-18 05:23:37 +0700

    convert class component to functional component

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {
   TextInput,
   StyleSheet,
@@ -17,148 +9,138 @@ import {
 } from 'react-native';
 import NotifService from './NotifService';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const App = () => {
+  const [registerToken, setRegisterToken] = useState('');
+  const [fcmRegistered, setFcmRegistered] = useState(false);
 
-    this.notif = new NotifService(
-      this.onRegister.bind(this),
-      this.onNotif.bind(this),
-    );
-  }
+  const onRegister = (token) => {
+    setRegisterToken(token.token);
+    setFcmRegistered(true);
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Example app react-native-push-notification
-        </Text>
-        <View style={styles.spacer}></View>
-        <TextInput
-          style={styles.textField}
-          value={this.state.registerToken}
-          placeholder="Register token"
-        />
-        <View style={styles.spacer}></View>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.localNotif();
-          }}>
-          <Text>Local Notification (now)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.localNotif('sample.mp3');
-          }}>
-          <Text>Local Notification with sound (now)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.scheduleNotif();
-          }}>
-          <Text>Schedule Notification in 30s</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.scheduleNotif('sample.mp3');
-          }}>
-          <Text>Schedule Notification with sound in 30s</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.cancelNotif();
-          }}>
-          <Text>Cancel last notification (if any)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.cancelAll();
-          }}>
-          <Text>Cancel all notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.checkPermission(this.handlePerm.bind(this));
-          }}>
-          <Text>Check Permission</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.requestPermissions();
-          }}>
-          <Text>Request Permissions</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.abandonPermissions();
-          }}>
-          <Text>Abandon Permissions</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.getScheduledLocalNotifications((notifs) =>
-              console.log(notifs),
-            );
-          }}>
-          <Text>Console.Log Scheduled Local Notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.getDeliveredNotifications((notifs) =>
-              console.log(notifs),
-            );
-          }}>
-          <Text>Console.Log Delivered Notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.createOrUpdateChannel();
-          }}>
-          <Text>Create or update a channel</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.popInitialNotification();
-          }}>
-          <Text>popInitialNotification</Text>
-        </TouchableOpacity>
-
-        <View style={styles.spacer}></View>
-
-        {this.state.fcmRegistered && <Text>FCM Configured !</Text>}
-
-        <View style={styles.spacer}></View>
-      </View>
-    );
-  }
-
-  onRegister(token) {
-    this.setState({registerToken: token.token, fcmRegistered: true});
-  }
-
-  onNotif(notif) {
+  const onNotif = (notif) => {
     Alert.alert(notif.title, notif.message);
-  }
+  };
 
-  handlePerm(perms) {
+  const notif = new NotifService(onRegister, onNotif);
+
+  const handlePerm = (perms) => {
     Alert.alert('Permissions', JSON.stringify(perms));
-  }
-}
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Example app react-native-push-notification
+      </Text>
+      <View style={styles.spacer}></View>
+      <TextInput
+        style={styles.textField}
+        value={registerToken}
+        placeholder="Register token"
+      />
+      <View style={styles.spacer}></View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.localNotif();
+        }}>
+        <Text>Local Notification (now)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.localNotif('sample.mp3');
+        }}>
+        <Text>Local Notification with sound (now)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.scheduleNotif();
+        }}>
+        <Text>Schedule Notification in 30s</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.scheduleNotif('sample.mp3');
+        }}>
+        <Text>Schedule Notification with sound in 30s</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.cancelNotif();
+        }}>
+        <Text>Cancel last notification (if any)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.cancelAll();
+        }}>
+        <Text>Cancel all notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.checkPermission(handlePerm());
+        }}>
+        <Text>Check Permission</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.requestPermissions();
+        }}>
+        <Text>Request Permissions</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.abandonPermissions();
+        }}>
+        <Text>Abandon Permissions</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.getScheduledLocalNotifications((notifs) => console.log(notifs));
+        }}>
+        <Text>Console.Log Scheduled Local Notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.getDeliveredNotifications((notifs) => console.log(notifs));
+        }}>
+        <Text>Console.Log Delivered Notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.createOrUpdateChannel();
+        }}>
+        <Text>Create or update a channel</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.popInitialNotification();
+        }}>
+        <Text>popInitialNotification</Text>
+      </TouchableOpacity>
+
+      <View style={styles.spacer}></View>
+
+      {fcmRegistered && <Text>FCM Configured !</Text>}
+
+      <View style={styles.spacer}></View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -197,3 +179,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
+export default App;
